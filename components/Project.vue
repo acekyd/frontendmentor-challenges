cc03a727eb3a26e00fb2548e07bcb97f5a3a2957
Adewale Abati <acekyd01@gmail.com>
2021-11-09 18:46:25 +0100

    initial commit

---
+++ components/Project.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="project">
+    This is the project
+    <div class="project-image">
+      <img :src="'~/assets/' + image" />
+    </div>
+    <div class="project__details">
+      <span class="project__details--title">
+        {{ title }}
+      </span>
+      <span class="project__details--status">
+        {{ status }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
